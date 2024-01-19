838b7b70ddd2212e37934a9d52581f9a394d6455
Ayman-SIDQI <Sidqi.Ayman.96@gmail.com>
2024-01-20 00:17:06 +0100

    My parsing done need to add chwia d ml7a w kamon

---
+++ confPars/main.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asidqi <asidqi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/03 19:46:31 by asidqi            #+#    #+#             */
+/*   Updated: 2024/01/19 22:54:56 by asidqi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ServerInf.hpp"
+
+
+
+int main(int ac, char *av[])
+{
+	if (ac != 2)
+		return (1);
+	std::vector<ServerInf> sInf;
+	ServerInf tmp;
+	std::ifstream inFile(av[1]);
+	std::string el;
+
+	if (inFile.fail())
+		return (1);
+	std::string line;
+	try
+	{
+
+		while (std::getline(inFile, line))
+		{
+			std::stringstream ss(line);
+			ss >> el;
+			if (el == "server")
+			{
+				el.clear();
+				ss >> el;
+				std::getline(inFile, line);
+				if (line == "{")
+					line = tmp.filltmp(inFile);
+				if (ServerInf::isbrac(line, '}'))
+				{
+					sInf.push_back(tmp);
+					tmp.reset();
+					continue;
+				}
+				else if (!el.empty())
+				{
+					tmp.reset();
+					throw "Unknown element";
+					break;
+				}
+			}
+		}
+		inFile.close();
+	}
+	catch (char const *e)
+	{
+		std::cout << e << '\n';
+	}
+	std::cout << "====================== " << std::endl;
+	for (std::vector<ServerInf>::iterator it = sInf.begin(); it != sInf.end(); ++it)
+	{
+		(*it).print();
+		std::cout << "intcrement: "<<(*it).locs.size()<<"\n";
+		std::cout << "size of serverinf: "<< sInf.size() <<"\n";
+	}
+	std::cout << "====================== " << std::endl;
+	return (0);
+}
